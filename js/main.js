a16b77f86ff03ef9168d3775a29713e0283e89b1
Daniel Stewardson <84638889+DanielStewardson@users.noreply.github.com>
2021-08-14 00:06:33 +0100

    Added about section on landing page. Slide in and fade transitions to elements across site. button hover translate position

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/*---------------------------- Page transitions -------------------------------*/
+
 window.onload = () => {
     const transition_item = document.querySelector('.transition-1');
     const transition_item_2 = document.querySelector('.transition-2');
@@ -24,3 +26,75 @@ window.onload = () => {
     }
 };
 
+/*----------------------------- Slide in elements --------------------------------*/
+
+const slideInStuff = document.querySelectorAll('.slide-in-left, .slide-in-right, .fade-in');
+
+function checkSlide(e) {
+
+    slideInStuff.forEach(slide => {
+        // half way through image
+        const slideInAt = (window.scrollY + window.innerHeight);
+        console.log(slideInAt)
+        //bottom of slide item
+        const itemBottom = slide.offsetTop + slide.clientHeight;
+        console.log(itemBottom)
+        //half way through the item
+        const isHalfShown = slideInAt > slide.offsetTop;
+        
+        const isNoScrolledPast = window.scrollY < itemBottom;
+        
+        if(isHalfShown && isNoScrolledPast) {
+            slide.classList.add('active');
+            } else {
+            slide.classList.remove('active');
+            }
+            })
+            
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
+
+/*----------------------------- De-bounce function --------------------------------*/
+
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+  /*---------------------------- Disable scroll when menu open -------------------------------*/
+
+const navButt = document.querySelector('input[name=nav-butt]');
+
+navButt.addEventListener('change', e => {
+    if (navButt.checked) {
+        disableScroll();
+    } else {
+        enableScroll();
+        }
+    })
+
+function disableScroll() {
+    // Get the current page scroll position
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+  
+        // if any scroll is attempted, set this to the previous value
+        window.onscroll = function() {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+}
+
+function enableScroll() {
+    window.onscroll = function() {};
+}
