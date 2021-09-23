36c20b8bfec7b4a0d4355af8c9737c232c3de54d
Daniel Stewardson <84638889+DanielStewardson@users.noreply.github.com>
2021-09-23 22:54:40 +0100

    changed desing. again. will never finish.

--- js/main.js
+++ js/main.js
@@ -4,18 +4,11 @@ window.onload = () => {
     const transition_item = document.querySelector('.transition-1');
     const transition_item_2 = document.querySelector('.transition-2');
     const anchors = document.querySelectorAll('.slide');
-    const switchBubble = document.querySelector('.switch-bubble');
 
     setTimeout(() => {
         transition_item.classList.remove('is-active');
         transition_item_2.classList.remove('is-active-2');
     }, 300);
-
-    switchBubble.style.display = 'block';
-
-    setTimeout(() => {
-      switchBubble.style.display = null;
-  }, 3000);
 
     for (let i = 0; i < anchors.length; i++) {
         const anchor = anchors[i];
@@ -81,14 +74,11 @@ function enableScroll() {
 
 const togButton = document.getElementById("retro-mode-butt");
 const settings = document.querySelector(':root');
-const gitIcon = document.querySelector('.github-icon-1');
-const gitIcon2 = document.querySelector('.github-icon-2');
 const links = document.querySelector('a');
 const html = document.querySelector('html');
 const coin = document.querySelector('.coin-switch');
-const switchBub1 = document.querySelector('.switch-bubble-1');
-const switchBub2 = document.querySelector('.switch-bubble-2');
-
+const backgroundImage = document.querySelector('.background-image');
+const navButton = document.querySelector('.nav-button-label');
 
 let darkOn = false;
 function toggle() {
@@ -105,23 +95,19 @@ function setTheme() {
   localStorage.setItem("dark", darkOn ? "true" : "false");
   if(darkOn){
     settings.setAttribute("theme", "boring");
-    gitIcon.setAttribute("theme", "boring");
-    gitIcon2.setAttribute("theme", "boring");
     links.setAttribute("theme", "boring");
     html.setAttribute("theme", "boring");
-    switchBub1.classList.remove('active');
-    switchBub2.classList.add('active');
-    coin.style.transform = 'translateX(40px) rotate(180deg)';
+    coin.style.transform = 'translateY(30px) rotate(180deg)';
+    backgroundImage.setAttribute('theme', 'boring');
+    navButton.setAttribute('theme', 'boring');
   }
   else{
     settings.setAttribute("theme", "retro");
-    gitIcon.setAttribute("theme", "retro");
-    gitIcon2.setAttribute("theme", "retro");
     links.setAttribute("theme", "retro");
     html.setAttribute("theme", "retro");
-    switchBub1.classList.add('active');
-    switchBub2.classList.remove('active');
     coin.style.transform = 'translateX(0)';
+    backgroundImage.setAttribute('theme', 'retro');
+    navButton.setAttribute('theme', 'retro');
   }
 }
 
